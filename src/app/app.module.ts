6acb6d7661bf6dc0573d308940bcca06140564d8
harold <haroldalvarez800@gmail.com>
2022-11-03 15:28:33 -0500

    3-11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BuyComponent } from './buy/buy.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { EventsComponent } from './events/events.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerGridComponent } from './banner-grid/banner-grid.component';
+import { MenuSliderComponent } from './menu-slider/menu-slider.component';
+import { MobilComponent } from './mobil/mobil.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { ScrollUpComponent } from './scroll-up/scroll-up.component';
+import { YandexMapComponent } from './yandex-map/yandex-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BuyComponent
+    NavbarComponent,
+    BannerComponent,
+    EventsComponent,
+    FooterComponent,
+    BannerGridComponent,
+    MenuSliderComponent,
+    MobilComponent,
+    ReservationComponent,
+    ScrollUpComponent,
+    YandexMapComponent
   ],
   imports: [
     BrowserModule,
